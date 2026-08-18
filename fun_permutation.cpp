5a53003a3c0c9cd506a8b5a26e71057ce8a53734
Vara693 <pvaradraj2@gmail.com>
2026-08-18 23:39:49 +0530

    Added daily solution 16

---
+++ fun_permutation.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> p(n);
+        for (int i=0; i<n; i++) {
+            cin >> p[i];
+        }
+
+        vector<int> q(n);
+        for (int i=0; i<n; i++) {
+            q[i] = n+1-p[i];
+            cout << q[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
